5a26db785950f0f2807fc7320e302b997d982c0d
sserges <agounkeserge@gmail.com>
2020-07-07 17:49:12 +0000

    Search Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import Header from './components/ui/Header';
 import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
 
 import './App.css';
 
@@ -26,6 +27,7 @@ const App = () => {
   return (
     <div className='container'>
       <Header />
+      <Search />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
